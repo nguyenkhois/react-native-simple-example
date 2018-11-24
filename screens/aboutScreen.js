722450656c426c7a1a06b51ec387cca139a98c66
Khoi Le <minarek@outlook.com>
2018-11-24 17:53:47 +0100

    Updated About component

--- screens/aboutScreen.js
+++ screens/aboutScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import styles from '../styles';
+
 export default class AboutScreen extends React.Component {
     render(){
         return(
             <View>
-                <Text>
-                    About screen
+                <Text style={styles.aboutSection}>
+                    The simple React Native example is using Expo. You can view the online demo on Expo or the source on GitHub.
+                </Text>
+
+                <Text style={styles.aboutSection}>
+                    You can view more information in the /README.md file.
+                </Text>
+
+                <Text style={styles.aboutSection}>
+                    Thanks for your visit! (^_^)
                 </Text>
             </View>
         );
