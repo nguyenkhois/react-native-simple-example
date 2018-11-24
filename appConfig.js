dd5932998f90cfdcaefc346a3634bf31c6c80d7c
Khoi Le <minarek@outlook.com>
2018-11-24 15:59:01 +0100

    Updated data resource

--- appConfig.js
+++ appConfig.js
@@ -1,6 +1,4 @@
 export const APIConfig = {
-    baseURL: 'https://www.vlexikon.com/demo/data',
-    products: 'https://www.vlexikon.com/demo/data/products.json',
-    categories: 'https://www.vlexikon.com/demo/data/categories.json',
-    imageURL: 'https://www.vlexikon.com/demo/data'
+    products: 'https://nguyenkhois.github.io/react-native-simple-example/assets/data/products.json',
+    imageURL: 'https://nguyenkhois.github.io/react-native-simple-example/assets/data'
 }
